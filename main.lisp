64129f769ba2ac3a5fde9db21c1f9f5ba4c4f2e8
erin-online <104870564+erin-online@users.noreply.github.com>
2024-10-11 02:51:36 -0500

    some work on feedback
    get-class-rules, random-exclude, print-feedback all complete. give-feedback works for a couple forms of feedback so far

--- main.lisp
+++ main.lisp
@@ -8,7 +8,7 @@
    (feature-names :initarg :feature-names :initform NIL :accessor feature-names
 		  :documentation "List of feature names, which get assigned in order. The list's length must be equal to num-features. Optional.")
    (cname :initarg :cname :initform NIL :accessor cname
-	       :documentation "The name of the class this rule is trying to observe. Should be a string. Not mandatory.")
+	  :documentation "The name of the class this rule is trying to observe. Should be a string. Not mandatory.")
    (memory :initarg :mem :initform NIL :accessor mem
 	       :documentation "Memory values of the rule's feature clauses. Should be handled by initialize-instance, but can be overridden by an initial value. Should be a collection of ints.")))
 
@@ -42,7 +42,9 @@
 	    (if (feature-names rule)
 		(concatenate 'string (write-to-string clause) " (" (elt (feature-names rule) clause) ")")
 		(write-to-string clause))
-	    (elt (mem rule) clause))))
+	    (elt (mem rule) clause)))
+    ; print newline at the end
+    (format t "~%"))
 
 (defmethod eval-rule ((rule rule) input)
   (dotimes (feature (num-features rule) T)
@@ -98,8 +100,88 @@
 					:cname (if (class-names tm) (elt (class-names tm) one-class) NIL))
 			 (rules tm)))))))
 
+(defmethod get-class-rules ((tm tm) class-id)
+  ; return vector of all rules that observe the given class-id
+  (let* ((lowest-index (loop for i upto (- class-id 1) sum (elt (rules-per-class tm) i)))
+	 (highest-index (+ lowest-index (elt (rules-per-class tm) class-id) -1)))
+    ; as i said on discord, we will probably store indices in the tm object so we don't have to calculate them every time
+    (make-array (elt (rules-per-class tm) class-id) :initial-contents (loop for i from lowest-index upto highest-index collect (elt (rules tm) i)))))
+
 (defmethod eval-tm ((tm tm) input &optional v)
   (let ((votes (make-array (num-rules tm) :fill-pointer 0)))
     (dotimes (rule (num-rules tm) votes)
       (if v (format t "Rule ~a returned ~a for class ~a.~%" rule (eval-rule (elt (rules tm) rule) input) (class-id (elt (rules tm) rule))))
       (vector-push (eval-rule (elt (rules tm) rule) input) votes))))
+
+(defun random-exclude (range excluded-num)
+  ; return random int in [0, range-1], except for excluded-num
+  (let ((num (random (1- range))))
+    (if (equal num excluded-num)
+	(1- range)
+	num)))
+
+(defun print-feedback (feature-num feature-present clause-polarity rule-num probability feedback-polarity feedback-success initial-clause-mem &optional roll clause-at-limit)
+  (format t "Feature ~a was ~a in the input, so the corresponding ~a clause in rule ~a may be ~a with prob. ~a.~%"
+	  (write-to-string feature-num)
+	  (if feature-present "present" "absent")
+	  (if clause-polarity "positive" "negative")
+	  (write-to-string rule-num)
+	  (if feedback-polarity "rewarded" "penalized")
+	  (write-to-string probability))
+  (if clause-at-limit
+      (format t "The clause was already at its ~a value, ~a, so no change was made.~%"
+	      (if feedback-polarity "maximum" "minimum")
+	      (write-to-string initial-clause-mem))
+      (progn
+	(format t "~ahe clause ~a receive a ~a.~%"
+		(if roll (format nil "~a was rolled, so t"
+				 (write-to-string roll))
+		    "T")
+		(if feedback-success "will" "won't")
+		(if feedback-polarity "reward" "penalty"))
+	(if feedback-success
+	    (format t "The clause was ~a, and its memorization value was moved from ~a to ~a.~%"
+		    (if feedback-polarity "rewarded" "penalized")
+		    (write-to-string initial-clause-mem)
+		    (write-to-string
+		     (if feedback-polarity
+			 (1+ initial-clause-mem)
+			 (1- initial-clause-mem)))))))
+  (format t "~%"))
+  
+
+(defmethod give-feedback ((tm tm) input-label input &optional v (spec (def-spec tm)) boost-positive)
+  ; counterexample stuff is currently unused, it's for the part of the feedback that isn't done yet
+  (let* ((counterexample-class (random-exclude (num-classes tm) input-label))
+	 (label-rules (get-class-rules tm input-label))
+	 (counterexample-rules (get-class-rules tm counterexample-class))
+	 ; todo: make it so we don't need to calculate indices every time
+	 (label-lowest-index (loop for i upto (- input-label 1) sum (elt (rules-per-class tm) i)))
+	 (counterex-lowest-index (loop for i upto (- counterexample-class 1) sum (elt (rules-per-class tm) i))))
+				       ; type 1 feedback, part 1
+    ; if a feature is present in the input, we loop through every rule matching the input's labelled class, and reward that feature's clauses with probability (s-1)/s
+    (dotimes (label-rule (length label-rules))
+      (dotimes (feature (num-features tm))
+	; set rand equal to random number in [0, specificity-1]. if we roll 0, then things with probability 1/s will happen. if we roll anything else, things with probability (s-1)/s will happen
+	(let ((rand (random spec))
+	      (init-clause-mem (elt (mem (elt label-rules label-rule)) feature)))
+	  (if (elt input feature)
+	      ; for each feature which is present in the input
+	      (progn
+		(if v
+		    ; dont worry about it lol
+		    (print-feedback feature t t (+ label-rule label-lowest-index) (if boost-positive 1 (/ (1- spec) spec)) t (not (equal rand 0)) init-clause-mem (if (not boost-positive) rand) (>= init-clause-mem (* 2 (num-states tm)))))
+		; reward with probability 1 if boost-positive is true. otherwise, reward with probability (s-1)/s, so do it if rand is not 0
+		(if (or boost-positive (not (equal rand 0)))
+		    (if (< init-clause-mem (* 2 (num-states tm)))
+			(incf (elt (mem (elt label-rules label-rule)) feature))))
+		; todo: penalize the corresponding negative clause with probability 1 (type 2 feedback). do it inside the below parenthesis
+		)
+	  ; for each feature which is absent in the input. this is the "else" in (if (elt input feature)) above if you're confused where we are
+	      (progn
+		(if v
+		    (print-feedback feature nil t (+ label-rule label-lowest-index) (/ 1 spec) nil (equal rand 0) init-clause-mem rand (<= init-clause-mem 1))
+		(if (equal rand 0)
+		    (if (> init-clause-mem 1)
+			(decf (elt (mem (elt label-rules label-rule)) feature))))))))))))
+    
