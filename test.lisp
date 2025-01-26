0e38a9efdab1f6929e3744839940a5a75fb46396
drea8 <45923854+drea8@users.noreply.github.com>
2025-01-25 22:02:02 -0600

    Update test.lisp

--- test.lisp
+++ test.lisp
@@ -1,28 +1,4 @@
 
-(defun class-test ()
-	;; Outdated, we now use bit vectors for inputs.
-  (defparameter *marxist-class-tm*
-    (make-instance 'tm
-		   :num-classes 2
-		   :num-features 10
-		   :def-spec 10
-		   :num-rules 10
-		   :rules-per-class '(5 5)
-		   :class-names '("Proletariat" "Bourgeoisie")
-		   :feature-names '("sells-labor-power"
-				    "receives-wages"	   		    
-				    "performs-manual-labor"
-				    "experiences-alienation"
-				    "potential-for-class-consciousness"	
-
-				    "owns-means-of-production"
-				    "controls-capital"
-				    "extracts-surplus-value"
-				    "has-political-power"				    
-				    "owns-private-property")))
-  (initialize-instance *marxist-class-tm*)
-  (eval-tm *marxist-class-tm* #(T T NIL NIL NIL T) :v))
-
 (defun xor-categorizer (data)
   ;; Returns 1 if exactly one of the two features is present, and 0 otherwise.
   (if (equal (elt data 0) (elt data 1)) 0 1))
